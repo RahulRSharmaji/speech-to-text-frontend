5db7b7b0072fd79c3ec35dea03553b82208da29f
RahulRSharmaji <rahul1998sharmaji@gmail.com>
2026-01-19 23:00:48 +0530

    initial commit react app with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Speech to Text</h1>
+        <p>Record or upload audio to convert it to text</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
